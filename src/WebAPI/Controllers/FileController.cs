fe4476b7c51d7c43996b7e055d47540af7888d07
Shady Obeed <shady.obeed@intel.com>
2023-01-06 11:57:40 +0200

    clean unneeded files

--- src/WebAPI/Controllers/FileController.cs
+++ src/WebAPI/Controllers/FileController.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers
 {
-    [Route("api/Ingredients")]
     [ApiController]
     public class FileController : Controller
     {
@@ -21,33 +20,6 @@ namespace WebAPI.Controllers
         public async Task<IFormFile> Sanitize(IFormFile file)
         {
             return await _mediator.Send(new ProcessFileCommand(file){});
-        }
-        
-        [HttpGet("SanitizeGet",Name = nameof(SanitizeGet))]
-        public async Task<string> SanitizeGet()
-        {
-            // Get the path to the desktop
-            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-
-            // Create the full path to the file
-            string filePath = Path.Combine(desktopPath, "test.txt");
-
-            // Create a new binary file
-            using (BinaryWriter writer = new BinaryWriter(System.IO.File.Open(filePath, FileMode.Create)))
-            {
-                // Write the first 3 bytes (123)
-                writer.Write((byte)1);
-                writer.Write((byte)2);
-                writer.Write((byte)3);
-
-                // Write the last 3 bytes (789)
-                writer.Seek(6, SeekOrigin.Begin);
-                writer.Write((byte)7);
-                writer.Write((byte)8);
-                writer.Write((byte)9);
-            }
-
-            return "finished";
         }
 
     }
